4681d8858c4f6585af4a7745b4847806d9ed0d23
deki90to <deki90to@gmail.com>
2021-03-15 00:45:39 +0100

    fixed home divs

--- tinder_app/urls.py
+++ tinder_app/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
 	path('<pk>/details/', views.details, name='details'),
 	path('<pk>/edit/', views.edit, name='edit'),
 	path('<pk>/delete/', views.delete, name='delete'),
-	path('create/', views.create, name='create'),
 ]
