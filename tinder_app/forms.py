4835e27eca8a639bd52c8c3007b19a27bebd13b5
deki90to <deki90to@gmail.com>
2021-03-15 00:28:36 +0100

    .

--- tinder_app/forms.py
+++ tinder_app/forms.py
@@ -6,3 +6,6 @@ class DogForm(forms.ModelForm):
 	class Meta:
 		model = Dog
 		fields = '__all__'
+		widgets = {
+			'user':forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'admin', 'type':'hidden'}),
+		}
