5678f3a5b7c7d80c9d1863b9f81bbd953021b1c6
deki90to <deki90to@gmail.com>
2021-03-15 00:22:11 +0100

    changed delete page, only user can change and delete own posts, problem with uploading image

--- tinder_app/models.py
+++ tinder_app/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django_resized import ResizedImageField
 from datetime import datetime
+from django.contrib.auth.models import User
+
+
+
 
 
 
@@ -13,20 +17,21 @@ class DogBreed(models.Model):
 
 
 class Dog(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	breed = models.ForeignKey('DogBreed', on_delete=models.CASCADE)
 	gender = models.CharField(max_length=10)
 	birthYear = models.CharField(max_length=15)
 	info = models.TextField(max_length=1000)
-	date = models.DateTimeField(default=datetime.now, blank=True)
-	image = ResizedImageField(size=[320,240], quality=100, upload_to='pictures', null=True)
+	image = ResizedImageField(size=[320,240], quality=100, upload_to='pictures', null=True, blank=False)
 	image2 = ResizedImageField(size=[320,240], quality=100, upload_to='pictures', null=True, blank=True)
 	image3 = ResizedImageField(size=[320,240], quality=100, upload_to='pictures', null=True, blank=True)
 	image4 = ResizedImageField(size=[320,240], quality=100, upload_to='pictures', null=True, blank=True)
 	image5 = ResizedImageField(size=[320,240], quality=100, upload_to='pictures', null=True, blank=True)
 	image6 = ResizedImageField(size=[320,240], quality=100, upload_to='pictures', null=True, blank=True)
+	date = models.DateTimeField(default=datetime.now, blank=True)
 
 	def __str__(self):
-		return (f'{self.birthYear}, {self.gender}, {self.breed}, {self.date}')
+		return (f'{self.birthYear}, {self.gender}, {self.breed}, {self.date}, ({self.user})')
 
 	class Meta:
 		ordering = ['-date']
